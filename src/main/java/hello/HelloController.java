2a1f30469213f0050975ddd73e19179431dbaa93
Icarus99 <40618471+Icarus99@users.noreply.github.com>
2019-10-07 15:02:07 -0700

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    String html = "<h1>Hello World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/mygithubid/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+return html;
     }
     
 }
